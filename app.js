174fe5b7f4738f3dfc3a6d4045162c75355c86e6
khomotso <kgomotso_15@live.com>
2023-11-10 10:42:40 +0200

    first chatbot commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function talk ()
+{
+    var know =
+    {
+        //key is the prompt recognised word from the user input.
+        //value is the response you get from the chatbot.
+
+        "Hello": "Hi there &128540",
+        "Who are you": "My name is Chatty Betty",
+        "what can you do for me": "Not much for now, just keeping you busy",
+        "ok": "you welcome",
+        "bye": "Goodbye chat later",
+        "Tell me about yourself": "I am just a smallanyana chat bot, just keeping you busy with HTML, CSS and JavaScript"
+    }
+}
